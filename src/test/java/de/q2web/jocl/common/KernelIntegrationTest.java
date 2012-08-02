6ab6494f986e7cc9c2a4eb55c6d81ddda77a0d3f
Oliver Schrenk <oliver.schrenk@gmail.com>
2012-08-02 17:49:50 +0200

    Add integration tests to compare execution time of pure java to jocl code

---
+++ src/test/java/de/q2web/jocl/common/KernelIntegrationTest.java
@@ -0,0 +1,60 @@
+package de.q2web.jocl.common;
+
+import static org.jocl.CL.CL_DEVICE_TYPE_GPU;
+import static org.jocl.CL.clReleaseCommandQueue;
+import static org.jocl.CL.clReleaseContext;
+
+import java.util.concurrent.TimeUnit;
+
+import org.jocl.cl_command_queue;
+import org.jocl.cl_context;
+import org.jocl.cl_device_id;
+import org.jocl.cl_platform_id;
+import org.jocl.utils.CommandQueues;
+import org.jocl.utils.Contexts;
+import org.jocl.utils.Devices;
+import org.jocl.utils.Platforms;
+import org.junit.Test;
+
+import com.google.common.base.Stopwatch;
+import com.google.common.primitives.Floats;
+
+import de.q2web.jocl.util.Arrays;
+import de.q2web.jocl.util.Duration;
+
+public class KernelIntegrationTest {
+
+	@SuppressWarnings("unused")
+	@Test
+	public void testMinimum() {
+		final cl_platform_id platformId = Platforms.getPlatforms().get(0);
+		final cl_device_id deviceId = Devices.getDevices(platformId,
+				CL_DEVICE_TYPE_GPU).get(0);
+		final cl_context context = Contexts.create(platformId, deviceId);
+		final cl_command_queue queue = CommandQueues.create(context, deviceId);
+		try {
+			final float[] floats = Arrays.random(10000000, -180, 180);
+			final Stopwatch stopwatch = new Stopwatch();
+
+			stopwatch.start();
+			final float minimumJava = Floats.min(floats);
+			final long elapsedTimeJava = stopwatch
+					.elapsedTime(TimeUnit.NANOSECONDS);
+			stopwatch.reset();
+
+			stopwatch.start();
+			final float minimumJocl = Kernels.minimum(context, queue, floats);
+			final long elapsedTimeJocl = stopwatch
+					.elapsedTime(TimeUnit.NANOSECONDS);
+			stopwatch.reset();
+
+			System.out.println("Java: " + Duration.of(elapsedTimeJava));
+			System.out.println("JOCL: " + Duration.of(elapsedTimeJocl));
+
+		} finally {
+			clReleaseCommandQueue(queue);
+			clReleaseContext(context);
+		}
+	}
+
+}
