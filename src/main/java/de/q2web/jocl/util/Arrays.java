a301783149604e811441f06b90a2def467168168
Oliver Schrenk <oliver.schrenk@gmail.com>
2012-08-02 17:29:43 +0200

    FIll array with random {float,double} with lower/upper bound or gaussian

--- src/main/java/de/q2web/jocl/util/Arrays.java
+++ src/main/java/de/q2web/jocl/util/Arrays.java
@@ -1,10 +1,13 @@
 package de.q2web.jocl.util;
 
+import java.util.Random;
+
 /**
- * Offers static helper methods for arrays
+ * Offers static helper methods for arrays.
  */
 public class Arrays {
 
+	/** The Constant NOT_FOUND. */
 	private static final int NOT_FOUND = -1;
 
 	/**
@@ -16,8 +19,8 @@ public class Arrays {
 	 *            the value
 	 * @return the boolean[]
 	 */
-	public static boolean[] prefilled(int length, boolean value) {
-		boolean a[] = new boolean[length];
+	public static boolean[] prefilled(final int length, final boolean value) {
+		final boolean a[] = new boolean[length];
 		java.util.Arrays.fill(a, value);
 		return a;
 	}
@@ -31,8 +34,8 @@ public class Arrays {
 	 *            the value
 	 * @return the byte[]
 	 */
-	public static byte[] prefilled(int length, byte value) {
-		byte a[] = new byte[length];
+	public static byte[] prefilled(final int length, final byte value) {
+		final byte a[] = new byte[length];
 		java.util.Arrays.fill(a, value);
 		return a;
 	}
@@ -46,8 +49,8 @@ public class Arrays {
 	 *            the value
 	 * @return the char[]
 	 */
-	public static char[] prefilled(int length, char value) {
-		char a[] = new char[length];
+	public static char[] prefilled(final int length, final char value) {
+		final char a[] = new char[length];
 		java.util.Arrays.fill(a, value);
 		return a;
 	}
@@ -61,8 +64,8 @@ public class Arrays {
 	 *            the value
 	 * @return the float[]
 	 */
-	public static float[] prefilled(int length, float value) {
-		float a[] = new float[length];
+	public static float[] prefilled(final int length, final float value) {
+		final float a[] = new float[length];
 		java.util.Arrays.fill(a, value);
 		return a;
 	}
@@ -76,8 +79,8 @@ public class Arrays {
 	 *            the value
 	 * @return the double[]
 	 */
-	public static double[] prefilled(int length, double value) {
-		double a[] = new double[length];
+	public static double[] prefilled(final int length, final double value) {
+		final double a[] = new double[length];
 		java.util.Arrays.fill(a, value);
 		return a;
 	}
@@ -91,8 +94,8 @@ public class Arrays {
 	 *            the value
 	 * @return the short[]
 	 */
-	public static short[] prefilled(int length, short value) {
-		short a[] = new short[length];
+	public static short[] prefilled(final int length, final short value) {
+		final short a[] = new short[length];
 		java.util.Arrays.fill(a, value);
 		return a;
 	}
@@ -106,8 +109,8 @@ public class Arrays {
 	 *            the value
 	 * @return the int[]
 	 */
-	public static int[] prefilled(int length, int value) {
-		int a[] = new int[length];
+	public static int[] prefilled(final int length, final int value) {
+		final int a[] = new int[length];
 		java.util.Arrays.fill(a, value);
 		return a;
 	}
@@ -121,8 +124,8 @@ public class Arrays {
 	 *            the value
 	 * @return the long[]
 	 */
-	public static long[] prefilled(int length, long value) {
-		long a[] = new long[length];
+	public static long[] prefilled(final int length, final long value) {
+		final long a[] = new long[length];
 		java.util.Arrays.fill(a, value);
 		return a;
 	}
@@ -134,7 +137,7 @@ public class Arrays {
 	 *            the values
 	 * @return the int
 	 */
-	public static final int positionOfMinimum(byte[] values) {
+	public static final int positionOfMinimum(final byte[] values) {
 		byte min = Byte.MAX_VALUE;
 		int pos = NOT_FOUND;
 		for (int i = 0; i < values.length; i++) {
@@ -154,7 +157,7 @@ public class Arrays {
 	 *            the values
 	 * @return the int
 	 */
-	public static final int positionOfMinimum(short[] values) {
+	public static final int positionOfMinimum(final short[] values) {
 		short min = Short.MAX_VALUE;
 		int pos = NOT_FOUND;
 		for (int i = 0; i < values.length; i++) {
@@ -174,7 +177,7 @@ public class Arrays {
 	 *            the values
 	 * @return the int
 	 */
-	public static final int positionOfMinimum(int[] values) {
+	public static final int positionOfMinimum(final int[] values) {
 		int min = Integer.MAX_VALUE;
 		int pos = NOT_FOUND;
 		for (int i = 0; i < values.length; i++) {
@@ -194,7 +197,7 @@ public class Arrays {
 	 *            the values
 	 * @return the int
 	 */
-	public static final int positionOfMinimum(long[] values) {
+	public static final int positionOfMinimum(final long[] values) {
 		long min = Long.MAX_VALUE;
 		int pos = NOT_FOUND;
 		for (int i = 0; i < values.length; i++) {
@@ -214,7 +217,7 @@ public class Arrays {
 	 *            the values
 	 * @return the int
 	 */
-	public static final int positionOfMinimum(float[] values) {
+	public static final int positionOfMinimum(final float[] values) {
 		float min = Float.MAX_VALUE;
 		int pos = -1;
 		for (int i = 0; i < values.length; i++) {
@@ -234,7 +237,7 @@ public class Arrays {
 	 *            the values
 	 * @return the int
 	 */
-	public static final int positionOfMinimum(double[] values) {
+	public static final int positionOfMinimum(final double[] values) {
 		double min = Double.MAX_VALUE;
 		int pos = NOT_FOUND;
 		for (int i = 0; i < values.length; i++) {
@@ -254,7 +257,7 @@ public class Arrays {
 	 *            the values
 	 * @return the int
 	 */
-	public static final int positionOfMaximum(byte[] values) {
+	public static final int positionOfMaximum(final byte[] values) {
 		byte max = Byte.MIN_VALUE;
 		int pos = NOT_FOUND;
 		for (int i = 0; i < values.length; i++) {
@@ -274,7 +277,7 @@ public class Arrays {
 	 *            the values
 	 * @return the int
 	 */
-	public static final int positionOfMaximum(short[] values) {
+	public static final int positionOfMaximum(final short[] values) {
 		short max = Short.MIN_VALUE;
 		int pos = NOT_FOUND;
 		for (int i = 0; i < values.length; i++) {
@@ -294,7 +297,7 @@ public class Arrays {
 	 *            the values
 	 * @return the int
 	 */
-	public static final int positionOfMaximum(int[] values) {
+	public static final int positionOfMaximum(final int[] values) {
 		int max = Integer.MIN_VALUE;
 		int pos = NOT_FOUND;
 		for (int i = 0; i < values.length; i++) {
@@ -314,7 +317,7 @@ public class Arrays {
 	 *            the values
 	 * @return the int
 	 */
-	public static final int positionOfMaximum(long[] values) {
+	public static final int positionOfMaximum(final long[] values) {
 		long max = Long.MIN_VALUE;
 		int pos = NOT_FOUND;
 		for (int i = 0; i < values.length; i++) {
@@ -334,7 +337,7 @@ public class Arrays {
 	 *            the values
 	 * @return the int
 	 */
-	public static final int positionOfMaximum(float[] values) {
+	public static final int positionOfMaximum(final float[] values) {
 		float max = Float.MIN_VALUE;
 		int pos = NOT_FOUND;
 		for (int i = 0; i < values.length; i++) {
@@ -354,7 +357,7 @@ public class Arrays {
 	 *            the values
 	 * @return the int
 	 */
-	public static final int positionOfMaximum(double[] values) {
+	public static final int positionOfMaximum(final double[] values) {
 		double max = Double.MIN_VALUE;
 		int pos = NOT_FOUND;
 		for (int i = 0; i < values.length; i++) {
@@ -367,4 +370,89 @@ public class Arrays {
 		return pos;
 	}
 
+	/**
+	 * Fill an array with random floats between a lower and an upper bound.
+	 * 
+	 * @param length
+	 *            the length
+	 * @param lowerBound
+	 *            the lower bound
+	 * @param upperBound
+	 *            the upper bound
+	 * @return the float[]
+	 */
+	public static float[] random(final int length, final float lowerBound,
+			final float upperBound) {
+		final Random random = new Random();
+		final float values[] = new float[length];
+		for (int i = 0; i < values.length; i++) {
+			values[i] = random.nextFloat() * (upperBound - lowerBound)
+					+ lowerBound;
+		}
+		return values;
+	}
+
+	/**
+	 * Fill an array with random doubles between a lower and an upper bound..
+	 * 
+	 * @param length
+	 *            the length
+	 * @param lowerBound
+	 *            the lower bound
+	 * @param upperBound
+	 *            the upper bound
+	 * @return the double[]
+	 */
+	public static double[] random(final int length, final double lowerBound,
+			final double upperBound) {
+		final Random random = new Random();
+		final double values[] = new double[length];
+		for (int i = 0; i < values.length; i++) {
+			values[i] = random.nextDouble() * (upperBound - lowerBound)
+					+ lowerBound;
+		}
+		return values;
+	}
+
+	/**
+	 * Fill an array with random floats that conform to gaussian distribution.
+	 * 
+	 * @param length
+	 *            the length
+	 * @param mean
+	 *            the mean
+	 * @param variance
+	 *            the variance
+	 * @return the float[]
+	 */
+	public static float[] randomGaussian(final int length, final float mean,
+			final float variance) {
+		final Random random = new Random();
+		final float values[] = new float[length];
+		for (int i = 0; i < values.length; i++) {
+			values[i] = (float) (mean + random.nextGaussian() * variance);
+		}
+		return values;
+	}
+
+	/**
+	 * Fill an array with random doubles that conform to gaussian distribution.
+	 * 
+	 * @param length
+	 *            the length
+	 * @param mean
+	 *            the mean
+	 * @param variance
+	 *            the variance
+	 * @return the double[]
+	 */
+	public static double[] randomGaussian(final int length, final double mean,
+			final double variance) {
+		final Random random = new Random();
+		final double values[] = new double[length];
+		for (int i = 0; i < values.length; i++) {
+			values[i] = (mean + random.nextGaussian() * variance);
+		}
+		return values;
+	}
 }
