21b4ede666599dc20987e8863eae99055e96aec2
Oliver Schrenk <oliver.schrenk@gmail.com>
2012-08-13 22:28:50 +0200

    Add JavaDoc

--- src/main/java/de/q2web/jocl/util/Duration.java
+++ src/main/java/de/q2web/jocl/util/Duration.java
@@ -3,26 +3,32 @@ package de.q2web.jocl.util;
 import java.util.concurrent.TimeUnit;
 
 /**
+ * Handle durations.
  *
  * @author Oliver Schrenk <oliver.schrenk@q2web.de>
  */
-//@formatter:off
 public class Duration {
 
+	/**
+	 * Returns the elapsed time formatted as <code>%ds, %dms, %dum, %dns</code>
+	 *
+	 * @param nanoseconds
+	 * @return the elapsed time formatted as <code>%ds, %dms, %dum, %dns</code>
+	 */
 	public static String of(final long nanoseconds) {
-
+//@formatter:off
 		return String.format(
-				"%ds, %dms, %dum, %dns",
-				// seconds
-				TimeUnit.NANOSECONDS.toSeconds(nanoseconds),
-				// milliseconds
-				TimeUnit.NANOSECONDS.toMillis(nanoseconds) 	-
-				TimeUnit.SECONDS.toMillis(TimeUnit.NANOSECONDS.toSeconds(nanoseconds)),
-				//microseconds
-				TimeUnit.NANOSECONDS.toMicros(nanoseconds)
-				- TimeUnit.MILLISECONDS.toMicros(TimeUnit.NANOSECONDS.toMillis(nanoseconds)),
-				//nanoseconds
-				nanoseconds - TimeUnit.MICROSECONDS.toNanos(TimeUnit.NANOSECONDS.toMicros(nanoseconds)));
+			"%ds, %dms, %dum, %dns",
+			// seconds
+			TimeUnit.NANOSECONDS.toSeconds(nanoseconds),
+			// milliseconds
+			TimeUnit.NANOSECONDS.toMillis(nanoseconds) 	-
+			TimeUnit.SECONDS.toMillis(TimeUnit.NANOSECONDS.toSeconds(nanoseconds)),
+			//microseconds
+			TimeUnit.NANOSECONDS.toMicros(nanoseconds)
+			- TimeUnit.MILLISECONDS.toMicros(TimeUnit.NANOSECONDS.toMillis(nanoseconds)),
+			//nanoseconds
+			nanoseconds - TimeUnit.MICROSECONDS.toNanos(TimeUnit.NANOSECONDS.toMicros(nanoseconds)));
 	}
 }
 //@formatter:on
