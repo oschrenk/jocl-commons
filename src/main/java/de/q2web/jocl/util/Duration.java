639675c7efea00ab176c561c98960ef907a17fbe
Oliver Schrenk <oliver.schrenk@gmail.com>
2012-08-13 22:25:01 +0200

    Format code

--- src/main/java/de/q2web/jocl/util/Duration.java
+++ src/main/java/de/q2web/jocl/util/Duration.java
@@ -3,7 +3,7 @@ package de.q2web.jocl.util;
 import java.util.concurrent.TimeUnit;
 
 /**
- * 
+ *
  * @author Oliver Schrenk <oliver.schrenk@q2web.de>
  */
 //@formatter:off
@@ -13,10 +13,16 @@ public class Duration {
 
 		return String.format(
 				"%ds, %dms, %dum, %dns",
+				// seconds
 				TimeUnit.NANOSECONDS.toSeconds(nanoseconds),
-				TimeUnit.NANOSECONDS.toMillis(nanoseconds) 	- TimeUnit.SECONDS.toMillis(TimeUnit.NANOSECONDS.toSeconds(nanoseconds)),
-				TimeUnit.NANOSECONDS.toMicros(nanoseconds) 	- TimeUnit.MILLISECONDS.toMicros(TimeUnit.NANOSECONDS.toMillis(nanoseconds)),
-				nanoseconds									- TimeUnit.MICROSECONDS.toNanos(TimeUnit.NANOSECONDS.toMicros(nanoseconds)));
+				// milliseconds
+				TimeUnit.NANOSECONDS.toMillis(nanoseconds) 	-
+				TimeUnit.SECONDS.toMillis(TimeUnit.NANOSECONDS.toSeconds(nanoseconds)),
+				//microseconds
+				TimeUnit.NANOSECONDS.toMicros(nanoseconds)
+				- TimeUnit.MILLISECONDS.toMicros(TimeUnit.NANOSECONDS.toMillis(nanoseconds)),
+				//nanoseconds
+				nanoseconds - TimeUnit.MICROSECONDS.toNanos(TimeUnit.NANOSECONDS.toMicros(nanoseconds)));
 	}
 }
 //@formatter:on
