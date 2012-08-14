3815336318054405301d9fe80f96624ae7062f78
Oliver Schrenk <oliver.schrenk@gmail.com>
2012-08-14 16:29:35 +0200

    Link to explnation of how Scanner work with boundary match

--- src/main/java/de/q2web/jocl/util/Resources.java
+++ src/main/java/de/q2web/jocl/util/Resources.java
@@ -11,6 +11,17 @@ import java.io.InputStream;
 public class Resources {
 
 	/**
+	 * @see <a
+	 *      href="http://weblogs.java.net/blog/pat/archive/2004/10/stupid_scanner_1.html">Stupid
+	 *      Scanner tricks...</a>
+	 * @see <a
+	 *      href="http://docs.oracle.com/javase/tutorial/essential/regex/bounds.html">Boundary
+	 *      Matchers</a>
+	 */
+	private static final String BOUNDARY_CONSTRUCT_BEGINNING_OF_INPUT = "\\A";
+	private static final String UTF_8 = "UTF-8";
+
+	/**
 	 * Convert <code>UTF-8</code> character based {@link InputStream} to
 	 * {@link String}.
 	 *
@@ -20,8 +31,8 @@ public class Resources {
 	 */
 	public static String convertStreamToString(final InputStream is) {
 		try {
-			return new java.util.Scanner(is, "UTF-8").useDelimiter("\\A")
-					.next();
+			return new java.util.Scanner(is, UTF_8).useDelimiter(
+					BOUNDARY_CONSTRUCT_BEGINNING_OF_INPUT).next();
 		} catch (final java.util.NoSuchElementException e) {
 			return "";
 		}
